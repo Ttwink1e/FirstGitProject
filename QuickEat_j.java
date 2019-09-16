95c65b9436812e856b43a62c08ad82af5816ef7c
Ttwink1e <55370254+Ttwink1e@users.noreply.github.com>
2019-09-16 11:16:42 +0800

    吃货联盟

---
+++ QuickEat_j.java
@@ -0,0 +1,18 @@
+package com.lxy.prepare;
+
+public class QuickEat_j {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		List lt = new List();
+//		String[] a = lt.getDish();
+//		for(String str:a){
+//			System.out.println(str);
+//		}
+		
+		List lt = new List();
+		lt.index_printf();
+	}
+
+}
